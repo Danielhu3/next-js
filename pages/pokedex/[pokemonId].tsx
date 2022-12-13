0bda556e6bb0de64aee3948c43596957ad07659d
Danielhu3 <danielhu3123@gmail.com>
2022-12-13 11:43:52 -0300

    rotas dinamicas

--- pages/pokedex/[pokemonId].tsx
+++ pages/pokedex/[pokemonId].tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
 
 const PokemonId = () => {
     const router = useRouter()
     const pokemonId = router.query.pokemonId
 
   return (
+    <>
     <div>pokemonId: {pokemonId}</div>
+    <Image 
+    src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokemonId}.png`}
+    alt={pokemonId ? pokemonId : ''}
+    width={475} height={475}
+    />
+    </>
   )
 }
 
